1d0a701176bcdb03a2bac389a42aac5c4f68e777
Daniel Bachler <daniel@danielbachler.de>
2019-08-23 14:22:17 +0200

    Split client into several files

---
+++ src/Client/Types.fs
@@ -0,0 +1,87 @@
+module Orn.Registry.Client.Types
+
+open Orn.Registry.Shared
+open Fable.PowerPack.Fetch
+open Orn.Registry
+
+type IKeycloakPromise<'returnType> =
+  abstract success : ('returnType -> Unit) -> IKeycloakPromise<'returnType>
+  abstract error : (obj -> Unit) -> IKeycloakPromise<'returnType>
+
+type IKeycloakPromiseWrapper<'returnType> =
+  abstract promise : IKeycloakPromise<'returnType>
+
+type IKeycloak =
+  abstract init : Unit -> IKeycloakPromise<bool>
+  abstract login : obj -> Unit
+  abstract token : string
+  abstract subject : string
+
+type OntologySearchTerm =
+  { Text : string
+    OntologyTerm : string option
+    TermSuggestions : string list
+  }
+
+type SearchStatus =
+  | NotStarted
+  | Loading
+  | Results of Shared.ServiceSqarqlQueryResult list
+
+type ServiceList =
+  | ServicesLoading
+  | ServicesError of string
+  | Services of Shared.ActiveServices
+
+type ActiveTab =
+  | ServicesTab
+  | SparqlQueryTab
+  | ExternalServices
+
+let AllTabs = [ ServicesTab ; SparqlQueryTab ; ExternalServices ]
+
+let tabToLabel = function
+| ServicesTab -> "Services"
+| SparqlQueryTab -> "SparQL query"
+| ExternalServices -> "External services"
+
+type AuthToken = AuthToken of string
+
+type LoginInfo =
+  { Token : AuthToken
+    UserId : string }
+
+type AppModel =
+  { Services : ServiceList
+    InputSearchTerm : OntologySearchTerm
+    OutputSearchTerm : OntologySearchTerm
+    SparqlQuery : string
+    SparqlResults : SparqlResultsForServices option
+    ActiveTab : ActiveTab
+    ExternalServiceTextFieldContent : string
+    SelectedSparqlService : string
+    LoginInfo : LoginInfo
+  }
+
+type Model =
+| Authenticating
+| LoggedIn of AppModel
+| LoginError of string
+
+type AppMsg =
+| Refresh of Result<Shared.ActiveServices, exn>
+| RunSparqlQuery
+| SparqlQueryFinished of Result<SparqlResultsForServices, exn>
+| Awake
+| QueryChanged of string
+| TabChanged of ActiveTab
+| AddExternalService
+| RemoveExternalService of string
+| ExternalServiceTextFieldChanged of string
+| AddExternalServiceRequestCompleted of Result<Response, exn>
+| RemoveExternalServiceRequestCompleted of Result<Response, exn>
+| SparqlSerivceSelected of string
+
+type Msg =
+| AppMessage of AppMsg
+| KeycloakInit of Result<LoginInfo, obj>
---
+++ src/Client/View.fs
@@ -0,0 +1,240 @@
+module Orn.Registry.Client.View
+
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+
+open Fulma
+
+
+open Orn.Registry.Shared
+open Fable.FontAwesome
+
+open Orn.Registry.Client.Types
+let appView (model : AppModel) (dispatch : AppMsg -> unit) =
+    let tabContent =
+      match model.ActiveTab with
+      | SparqlQueryTab ->
+          let results =
+            match model.SparqlResults with
+            | None -> []
+            | Some results ->
+                results
+                |> Array.toList
+                |> List.collect (fun result ->
+                      [   div [ ]
+                             ([ h5 [] [ str result.ServiceName]
+                                div [ ] [ a [ Href (result.OpenApiUrl.ToString()); Target "_blank" ] [ str "View OpenApi →" ]]
+                              ]
+                              @
+                              match result.Result with
+                              | BooleanResult resultvalue ->
+                                  [ div [] [ (if resultvalue then str "True" else str "False")] ]
+                              | BindingResult bindingresult ->
+                                  let headers = tr [] (bindingresult.Variables |> List.map (fun variable -> th [  ] [str variable] ))
+                                  let rows =
+                                    bindingresult.ResultValues
+                                    |> List.map (fun resultrow -> tr [] (resultrow |> List.map (fun cell -> td [] [ str cell])))
+
+                                  [ div [ ClassName "container"]
+                                      [
+                                        table [ ClassName "table"]
+                                          [ thead [] [headers]
+                                            tbody [] rows
+                                          ]
+                                      ]
+                                  ]
+                              | NoResult -> []
+                             )
+                      ]
+
+
+                    )
+          [ h3  [] [ str "Custom SparQL query" ]
+            div [ ClassName "form-group" ] <|
+              [ textarea [ Rows 10; Class "form-control"; OnChange (fun e -> dispatch (QueryChanged e.Value)) ; DefaultValue (model.SparqlQuery)] []
+              ]
+              @
+              match model.Services with
+              | ServicesLoading -> []
+              | ServicesError err -> [ ]
+              | Services {ExternalOrnServices = externalServices; OrnServices = ornServices; Messages = messages } ->
+                [ Fulma.Columns.columns []
+                    [ Fulma.Column.column [ Column.Option.Width(Screen.All, Column.IsOneThird) ] [ label [] [ str "Query only this service (optional): " ] ]
+                      Fulma.Column.column []
+                        [
+                          Fulma.Select.select [ Select.Option.IsFullWidth  ]
+                            [ select [ Value model.SelectedSparqlService; OnChange (fun event -> dispatch <| SparqlSerivceSelected event.Value) ]
+                                ( [ option [ Value "" ]  [ str "" ] ]
+                                  @
+                                  (
+                                    List.concat
+                                      [ externalServices |> List.map (fun service -> service.OpenApiServiceInformation)
+                                        ornServices |> List.map (fun service -> service.OpenApiServiceInformation)
+                                      ]
+                                    |> List.map (fun service ->
+                                          option [ Value service.OpenApiUrl ]  [ str service.Name ] ) )
+                                  ) ] ]
+                    ]
+                ]
+
+            button [ ClassName "btn btn-primary"; OnClick (fun _ -> dispatch RunSparqlQuery) ] [ str "Search" ]
+            h3 [] [ str "Results" ]
+          ]
+          @ (if List.isEmpty results then [  str "No results" ] else results)
+      | ExternalServices ->
+          match model.Services with
+          | ServicesLoading -> [ p [] [str "Loading ..."] ]
+          | ServicesError err -> [ p [] [str ("Error loading services: " + err)] ]
+          | Services {ExternalServices = externalServices; Messages = messages } ->
+              let externalServiceFragments =
+                  externalServices
+                  |> List.map (fun service ->
+                        div [ ClassName "row resource-listing__resource" ]
+                            [ div [ ClassName "resource__title" ]
+                                 [ str service ]
+                              Button.a [ Button.Option.OnClick (fun _ -> dispatch <| RemoveExternalService service ) ] [ Fa.i [ Fa.Solid.Trash ] [] ]
+                            ]
+                  )
+              let feedbackMessages =
+                  messages
+                  |> List.map (fun feedbackMessage ->
+                        div [  ]
+                            [( match feedbackMessage.Feedback with
+                               | OpenApiDownloadFailed (OpenApiUrl url) -> str (sprintf "Downloading OpenAPI failed from URL: %s" url)
+                               | OpenApiParsingFailed (OpenApiUrl url, openapiMessage) -> str (sprintf "Parsing OpenAPI failed (from URL: %s) with message: %s" url openapiMessage)
+                               | JsonLdContextMissing (OpenApiUrl url) -> str (sprintf "Json-LD context missing in OpenAPI definition at URL: %s" url)
+                               | JsonLdParsingError (OpenApiUrl url, jsonldMessage) -> str (sprintf "Json-LD parsing error (from URL: %s) with message: %s" url jsonldMessage)
+                            )]
+                    )
+
+              [ h3  [] [ str "Add a new external service" ]
+                Fulma.Text.p [ GenericOption.Modifiers [ Fulma.Modifier.TextSize(Screen.All, TextSize.Is7) ] ] [ str "Please beware that external services are not persisted at the moment (i.e. you have to add them again if the registry is restarted)"]
+
+                Fulma.Columns.columns []
+                  [
+                    Column.column [ Column.Option.Width(Screen.All, Column.IsFourFifths) ]
+                     [ Fulma.Input.text
+                        [ Input.Option.ValueOrDefault model.ExternalServiceTextFieldContent
+                          Input.Option.OnChange (fun event -> dispatch <| ExternalServiceTextFieldChanged event.Value )
+                          Input.Placeholder "Absolute URL of OpenRiskNet annotated OpenAPI definition"
+                         ]
+                     ]
+                    Column.column []
+                     [ Fulma.Button.button [ Button.OnClick (fun _ -> dispatch AddExternalService) ] [ str "Add service"]
+                     ]
+
+                  ]
+                h3  [] [ str "Currently registered external services" ]
+              ]
+              @
+              externalServiceFragments
+              @
+              [
+                h3  [] [ str "Recent registry messages: " ]
+                div [] feedbackMessages
+              ]
+      | ServicesTab ->
+          match model.Services with
+          | ServicesLoading -> [ p [] [str "Loading ..."] ]
+          | ServicesError err -> [ p [] [str ("Error loading services: " + err)] ]
+          | Services {PlainK8sServices = k8sServices; OrnServices = ornServices; ExternalOrnServices = externalServices; Messages = messages} ->
+              // TODO: render external services
+              let plainK8sFragments =
+                  k8sServices
+                  |> List.map (fun app ->
+                        div [ ClassName "media" ; Style [ Border "1px solid lightgrey" ; Padding "1em" ] ]
+                            [ div [ ClassName "media-body" ]
+                                [ h5 [ ClassName "mt-0" ]
+                                     [ str app.Name ]
+                                ]
+                            ])
+              let ornServiceFragments =
+                  ornServices
+                  |> List.map (fun app ->
+                        let swaggerUrl = app.OpenApiServiceInformation.OpenApiUrl.Unwrap()
+                        let swaggerUiLink = sprintf "/swaggerui?service=%s" (swaggerUrl |> Fable.Import.JS.encodeURIComponent)
+                        let rawOpenApiLink = sprintf "/openapi-raw?service=%s" (swaggerUrl |> Fable.Import.JS.encodeURIComponent)
+                        let dereferencedOpenApiLink = sprintf "/openapi-dereferenced?service=%s" (swaggerUrl |> Fable.Import.JS.encodeURIComponent)
+                        div [ ClassName "col-md-6" ]
+                            [ div [ ClassName "services-listing__service" ]
+                                [ div [ ClassName "service__name" ]
+                                     [ str app.OpenApiServiceInformation.Name ]
+                                  div [ ClassName "service__info service__info-item"]
+                                    [ div [ ClassName "service__info-label"] [ str "Indexed at "]
+                                      div [ ClassName "service__info-value"] [ str (app.OpenApiServiceInformation.RetrievedAt.ToString("yyyy-MMM-dd HH:mm:ss")) ]
+                                    ]
+                                  div [ ClassName "service__description"] [ str app.OpenApiServiceInformation.Description ]
+                                  br []
+                                  div [ ClassName "service__info" ]
+                                    ( app.OpenApiServiceInformation.Endpoints
+                                      |> List.map (fun endpoint -> div [ ClassName "service__info-item" ] [ str endpoint ]) )
+                                  div [ ClassName "service__more-links"]
+                                    [ a [ Href (rawOpenApiLink); Target "_blank" ] [ str "View raw OpenApi →" ]
+                                      br []
+                                      a [ Href (dereferencedOpenApiLink); Target "_blank" ] [ str "View dereferenced OpenApi →" ]
+                                      br []
+                                      a [ Href (swaggerUiLink); Target "_blank" ] [ str "View SwaggerUI →" ]
+                                    ]
+                                ]
+                            ]
+                  )
+              let externalServiceFragments =
+                  externalServices
+                  |> List.map (fun app ->
+                        let swaggerUrl = app.OpenApiServiceInformation.OpenApiUrl.Unwrap()
+                        let swaggerUiLink = sprintf "/openapi?service=%s" (swaggerUrl |> Fable.Import.JS.encodeURIComponent)
+                        div [ ClassName "col-md-6" ]
+                            [ div [ ClassName "services-listing__service" ]
+                                [ div [ ClassName "service__name" ]
+                                     [ str app.OpenApiServiceInformation.Name ]
+                                  div [ ClassName "service__description"] [ str app.OpenApiServiceInformation.Description ]
+                                  br []
+                                  div [ ClassName "service__info" ]
+                                    ( app.OpenApiServiceInformation.Endpoints
+                                      |> List.map (fun endpoint -> div [ ClassName "service__info-item" ] [ str endpoint ]) )
+                                  div [ ClassName "service__more-links"] [ a [ Href (swaggerUiLink); Target "_blank" ] [ str "View OpenApi →" ]]
+                                ]
+                            ]
+                  )
+              let feedbackMessages =
+                  messages
+                  |> List.map (fun feedbackMessage ->
+                        div [  ]
+                            [( match feedbackMessage.Feedback with
+                               | OpenApiDownloadFailed (OpenApiUrl url) -> str (sprintf "Downloading OpenAPI failed from URL: %s" url)
+                               | OpenApiParsingFailed (OpenApiUrl url, openapiMessage) -> str (sprintf "Parsing OpenAPI failed (from URL: %s) with message: %s" url openapiMessage)
+                               | JsonLdContextMissing (OpenApiUrl url) -> str (sprintf "Json-LD context missing in OpenAPI definition at URL: %s" url)
+                               | JsonLdParsingError (OpenApiUrl url, jsonldMessage) -> str (sprintf "Json-LD parsing error (from URL: %s) with message: %s" url jsonldMessage)
+                            )]
+                    )
+
+              [ h3  [] [ str "OpenRiskNet services running in the VRE" ]
+                div [ ClassName "row services-listing" ] ornServiceFragments
+                h3  [] [ str "External services with OpenRiskNet annotation" ]
+                div [ ClassName "row services-listing" ] externalServiceFragments
+                h3  [] [ str "Kubernetes services (debug view)" ]
+                div [] plainK8sFragments
+                h3  [] [ str "Recent registry messages: " ]
+                div [] feedbackMessages
+              ]
+
+    let renderTabs options labelFn activeTab message =
+      Tabs.tabs [ ]
+          (options
+           |> List.map (fun option ->
+                            Tabs.tab
+                                (if activeTab = option then [ Tabs.Tab.IsActive true] else [])
+                                [ a [ OnClick (fun _ -> dispatch (message option)) ] [ str (labelFn option) ] ] ) )
+    div []
+      [ renderTabs AllTabs tabToLabel model.ActiveTab TabChanged
+        div [] tabContent
+      ]
+
+let view (model : Model) (dispatch : Msg -> unit) =
+    match model with
+    | Authenticating ->
+      div [] [ str "Authenticating..."]
+    | LoggedIn appModel ->
+      appView appModel (AppMessage >> dispatch)
+    | LoginError error ->
+      div [] [ str "Error during login: "; str error ]
